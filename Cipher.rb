9c5ce6698f40277565546b641a8c046f46c80547
d00cky <83293500+Shledakayn@users.noreply.github.com>
2025-09-27 21:52:32 -0300

    Finished the logical part of the problem Finished the main core of the probelem that is the logical part, and then refine to catch empty spaces like ' ' and special characters to enter in the same index.

--- Cipher.rb
+++ Cipher.rb
@@ -1,21 +1,23 @@
 # DISPLAY a message to the user, asking the messsage and the key
-#puts "Welcome to Caesar Cipher"
-#puts "Type your message:\n" # store the message
-#message = gets.chomp
-#puts "How many shifts you want?\n" # store the key
-#key_shift = gets.chomp
+puts "Welcome to Caesar Cipher"
+puts "Type your message:\n" # store the message
+message = gets.chomp
+puts "How many shifts you want?\n" # store the key
+key_shift = gets.chomp
 
 cipher_alpha = [ 
 'A', 'B', 'C','D', 'E', 'F', 'G', 'H', 'I', 
 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ]
+encrypted_message = []
 
-message = "Something"
+# Take the message from the input and then criptography,
+# using the shifts the user has selected.
 message_array = message.chars
 message_array.each do |letter|
-  searching = cipher_alpha.find_index(letter.upcase) + 5
-  a = []
-  a << cipher_alpha[searching]
-  p a.join
+  searching = cipher_alpha.find_index(letter.upcase) + key_shift.to_i
+  encrypted_message << cipher_alpha[searching]
 end
+
+p encrypted_message.join.downcase!.capitalize!
